2b229dc90c6943243f62f6622eb49c112afd41d1
purushotham0106 <120293662+purushotham0106@users.noreply.github.com>
2023-10-04 14:55:12 +0530

    Add files via upload

--- Scripts/activate.fish
+++ Scripts/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'D:\final\venv'
+set -gx VIRTUAL_ENV 'C:\Users\Nishanth\PycharmProjects\final\ve'
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | head -c 1) -lt 3
